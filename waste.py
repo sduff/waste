3537eeed05ca28ef9a8502e2aee3987e24197746
Simon Duff <simon.duff@gmail.com>
2021-12-23 13:39:39 +0800

    site builder

--- waste.py
+++ waste.py
@@ -8,7 +8,7 @@ print(f"""                      __
 
 import os
 import chevron      # mustache template
-import mistletoe    # markdown renderer
+import mistune      # markdown
 import shutil       # for copying static files
 
 # Utility functions
@@ -34,7 +34,8 @@ for root, dirs, filenames in os.walk(TEMPLATE_PATH):
 # copy static files into public directory
 print("📄 Copying static files")
 def copy_tree_verbose(path, names):
-    print("\t", path, names)
+    for f in names:
+        print("\t", f )
     return []
 shutil.copytree(STATIC_FILES, OUTPUT_PATH, dirs_exist_ok=True, ignore=copy_tree_verbose) #py8 required
 
@@ -46,25 +47,57 @@ for root, dirs, filenames in os.walk(SOURCE_PATH):
     for filename in filenames:
         fn,ex = os.path.splitext(filename)
         fullpath = os.path.join(root, filename)
-        #print("\t",root, fn, ex, fullpath)
 
         obj = {}
+        content = ""
 
-        with open(fullpath, "r") as md:
-            # update markdown if requireed before rendering
+        # basic metadata
+        obj["src_path"] = fullpath
+        obj["mod_date"] = os.path.getmtime(fullpath)
 
-            # update mustache object
-            obj["md"] = mistletoe.markdown(md)
-            obj["template"] = "default"
+        src_file = open(fullpath, "r")
+        state = "metadata"
+        for line in src_file:
+            # read  metadata
+            if state == "metadata":
+                if line.startswith("---"):
+                    state = "content"
+                else:
+                    if ":" not in line:
+                        print("Error parsing metadata line (",line,") in ", fullpath)
+                        continue
+                    else:
+                        name, value = line.strip().split(":",1)
+                        name = name.strip()
+                        value = value.strip()
+                        if name == "tags":
+                            value = value.split(",")
+                        obj[name] = value
+            else:
+                content += line
 
-            # load the template
-            template = templates[obj["template"]]
+        # update markdown if requireed before rendering
+        # update mustache object
+        obj["md"] = content
+        obj["template"] = "default" if "template" not in obj else obj["template"]
 
-            # render the html
-            html = chevron.render(template, obj)
+        # load the template
+        template = templates[obj["template"]]
 
-            # test output
-            print("\t", fullpath)
-            #print(html)
+        # render the html
+        html = chevron.render(template, obj)
+
+        # create the output file
+        # /src/foo/bah.md -> /dest/foo/bah/index.md
+        # $filename -> /dest/$filename
+        dest_dir =  os.path.join(root, fn)
+        dest_dir = os.path.join(dest_dir, 'index.html')
+        dest_dir = dest_dir.replace(SOURCE_PATH,OUTPUT_PATH,1)
+        dest_dir = os.path.join(OUTPUT_PATH, obj["filename"]) if "filename" in obj else dest_dir
+        os.makedirs(os.path.dirname(dest_dir), exist_ok=True)
+        with open(dest_dir, "w") as dest_file:
+            dest_file.write(html)
+
+        print("\t",fullpath,"->", dest_dir)
 
 print("ALL DONE. BYE BYE")
